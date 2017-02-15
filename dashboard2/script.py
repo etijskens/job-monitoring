8392e0cdfa6a0f342920d2f2657a9073e4fab549
etijskens <engelbert.tijskens@uantwerpen.be>
2017-02-15 08:32:00 +0100

    script.py
    bug fix and clean up

--- dashboard2/script.py
+++ dashboard2/script.py
@@ -16,69 +16,21 @@ class Data_jobscript:
     def __init__(self,jobid,compute_node):
         self.compute_node = compute_node # must be mhost
         self.jobid = jobid
-        command = "ssh {} 'sudo cat /opt/moab/spool/torque/mom_priv/jobs/{}.hopper.SC'".format(compute_node,jobid)
-        if _test:
-            self.data = [ '#!/bin/bash'
-                        , ''
-                        , ' # "name" of the job (optional)'
-                        , ' #PBS -N BLACK-dftd3'
-                        , ''
-                        , ' # requested running time (required!)'
-                        , ' #PBS -l walltime=72:00:00'
-                        , ''
-                        , ' # specification (required!)'
-                        , ' #   nodes=   number of nodes; 1 for serial; 1 or more for parallel'
-                        , ' #   ppn=     number of processors per node; 1 for serial; up to 8'
-                        , ' #   if you want your "private" node: ppn=8'
-                        , ' #   mem=     memory required'
-                        , ''
-                        , '# for hopper'
-                        , '#PBS -l nodes=2:ppn=20'
-                        , ''
-                        , '== if, in addition, you want to avoid that other (yours) jobs of you also could'
-                        , '== use this node, also add'
-                        , '# #PBS -l naccesspolicy=singlejob'
-                        , '#PBS -W x=nmatchpolicy:exactnode'
-                        , ''
-                        , ' # send mail notification (optional)'
-                        , ' #   a        when job is aborted'
-                        , ' #   b        when job begins'
-                        , ' #   e        when job ends'
-                        , ' #   M        your e-mail address (should always be specified)'
-                        , ' #PBS -m e'
-                        , ' #PBS -M deniz.cakir@uantwerpen.be'
-                        , ''
-                        , '# go to the (current) working directory (optional, if this is the'
-                        , '# directory where you submitted the job)'
-                        , '#cd $PBS_O_WORKDIR'
-                        , ''
-                        , 'cd /scratch/antwerpen/201/vsc20164/blackP-rotated/dft-d3/perp-pressure/e-field/0.01 '
-                        , ''
-                        , ''
-                        , '# purge modules'
-                        , 'module purge'
-                        , ''
-                        , '# for hopper:'
-                        , 'module load VASP/5.3.5-intel-2014a'
-                        , ''
-                        , 'mpirun vasp > log-20'
-                        , ''
+        self.modules = None                    
+        try:
+            command = "ssh {} 'sudo cat /opt/moab/spool/torque/mom_priv/jobs/{}.hopper.SC'".format(compute_node,jobid)
+            self.data = remote.run(command,post_processor=remote.list_of_lines,attempts=1,raise_exception=True)            
+        except Exception as e:
+            print(type(e),e)
+            s = str(e)
+            self.data = None
+        if self.data is None:
+            self.data = ['# Jobscript not found by command: "{}"'.format(command)
+                        ,'# job may be completed already.'
                         ]
-        else:
-            try:
-                self.data = remote.run(command,post_processor=remote.list_of_lines,attempts=1,raise_exception=True)
-            except Exception as e:
-                print(type(e),e)
-                s = str(e)
-                if 'No such file or directory' in s:
-                    self.data = ['# '+s
-                                ,'# job may be completed already.'
-                                ]
-                    self.clean = self.data
-                return
+            self.clean = self.data
+            return
             
-        if len(self.data)==1 and not self.data[0]:
-            self.data[0] = '# Jobscript not found by command: "{}"'.format(command)
         # remove comment lines, empty lines
         self.clean = []
         for line in self.data:
@@ -89,7 +41,6 @@ class Data_jobscript:
                         self.clean.append(stripped) 
                 else:
                     self.clean.append(line)
-        self.modules = None                    
     #---------------------------------------------------------------------------    
     def __str__(self):
         """
