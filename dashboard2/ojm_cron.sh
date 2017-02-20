e69a4fcdece9d700392764f25f6c7014fe0b9ecc
etijskens <engelbert.tijskens@uantwerpen.be>
2017-02-20 16:14:42 +0100

    ojm_cron.sh
    now writes the start and stop time of every call to ojm_cron.py to
    ojm_cron.log

--- dashboard2/ojm_cron.sh
+++ dashboard2/ojm_cron.sh
@@ -9,7 +9,11 @@ cd /user/antwerpen/201/vsc20170/data/jobmonitor
 if [ ! -f ojm_cron.pickled ]; then
     date > ojm_cron.err
     rm -f ojm_cron.out
+    rm -f ojm_cron.log
 fi
-which python >> ojm_cron.out
+#which python >> ojm_cron.out
 
+START=$(date)
 python ojm_cron.py 2>> ojm_cron.err 1>> ojm_cron.out
+STOP=$(date)
+echo ${START} './ojm_cron.sh' ${STOP} >> ojm_cron.log
