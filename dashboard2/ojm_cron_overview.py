fee90dd4bfb3dc0024a31043ee133c200d05d701
etijskens <engelbert.tijskens@uantwerpen.be>
2017-02-23 12:18:11 +0100

    ojm_cron_overview.py
    get status of ojm_cron

--- dashboard2/ojm_cron_overview.py
+++ dashboard2/ojm_cron_overview.py
@@ -4,10 +4,9 @@ Script that provides an overview of the offline job monitor.
 import argparse
 import remote
 from titleline import title_line
-from is_ojm_running import is_ojm_running
 
 #===============================================================================    
-def ojm_cron_overview(only_new_completed_jobs=True):
+def ojm_cron_overview():
     """
     Main functIon of the script.
     Print overview of the offline job monitorl
@@ -19,7 +18,7 @@ def ojm_cron_overview(only_new_completed_jobs=True):
     :param [only_new_completed_jobs]: if *True* only new completed jobs with warnings are listed, otherwise old completed jobs are listed too. 
     
     """
-    command = 'cat data/jobmonitor/ojm.err'
+    command = 'cat data/jobmonitor/ojm_cron.err'
     print(title_line(command, width=100, char='-', above=True))
     lines = remote.run(command, post_processor=remote.list_of_lines)
     if lines:
@@ -28,23 +27,17 @@ def ojm_cron_overview(only_new_completed_jobs=True):
     print(title_line(width=100, char='-'))
         
     print(title_line('Completed jobs', width=100, char='-', above=True, below=True))
-    if only_new_completed_jobs:
-        command = 'cd data/jobmonitor/completed/ ; ls *.pickled'
-    else:
-        command = 'cd data/jobmonitor/completed/ ; ls *.pickle*'
+    command = 'ls data/jobmonitor/completed/'
     lines = remote.run(command,post_processor=remote.list_of_non_empty_lines,attempts=1,verbose=False)
     n = 0
     if lines:
         n = len(lines)
         for line in lines:
             print(line)
-    if only_new_completed_jobs:
-        print(n,'new completed jobs with warnings.\n')
-    else:
-        print(n,'completed jobs (new and old) with warnings.\n')
+    print(n,'new completed jobs with warnings.\n')
         
     print(title_line('Running jobs', width=100, char='-', above=True, below=True))
-    command = 'cd data/jobmonitor/running ; ls *.pickled'
+    command = 'ls data/jobmonitor/running/'
     lines = remote.run(command,post_processor=remote.list_of_non_empty_lines)
     for line in lines:
         print(line)
@@ -56,8 +49,8 @@ def ojm_cron_overview(only_new_completed_jobs=True):
 if __name__=='__main__':
     remote.connect_to_login_node()
 
-    parser = argparse.ArgumentParser('offline_overview')
-    parser.add_argument('--old','-o',action='store_true')
-    args = parser.parse_args()
-    print('offline_overview.py: command line arguments:',args)
-    offline_overview(not args.old)
+#     parser = argparse.ArgumentParser('offline_overview')
+#     parser.add_argument('--old','-o',action='store_true')
+#     args = parser.parse_args()
+    print('ojm_cron_overview.py')
+    ojm_cron_overview()
