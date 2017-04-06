14666dc0ba9426f7223dc82f70ae3a1b45982acb
etijskens <engelbert.tijskens@uantwerpen.be>
2017-04-06 15:09:08 +0200

    todo
    towards a job monitor that is useful for everybody, all admins and job
    owners

---
+++ dashboard2/todo.py
@@ -0,0 +1,9 @@
+"""
+* separate cron job and gui
+* config for admin or even user to get mail when sth goes wrong
+* add a gui button to prepare a mail to the job owner
+* develop criteria to send out mail for running jobs to admins, job owner
+* handle qstat -f warnings
+  which warnings exist? ask franky 
+* accumulate statistics
+"""
